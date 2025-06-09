f69dd91de9956e4ee965038e3f5bbec39bbc9d5e
Samiya Ali Zaidi <samiyaalizaidi@gmail.com>
2025-06-09 14:12:22 +0500

    initial styling

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+// import PropTypes from 'prop-types'
+// Notes:
+// - Template used by: `rafce` + enter
+// - PropTypes used by: `impt` + enter
+
+// import React from 'react' // dont need this anymore
+
+// arrow function component
+const Header = ({title = 'Task Tracker'}) => {
+  return (
+    <header>
+        <h1>{title}</h1>
+    </header>
+  )
+}
+
+// Header.defaultProps = {
+//     title: 'Task Tracker',
+// } -> not support for arrow functions anymore, use the default parameters example above
+
+
+// Header.propTypes = {
+//     title: PropTypes.string.isRequired,
+// } -> not supported anymore, will be ignored. use typescript if you want to use this
+
+// can be used for CSS styling inline for JSX tags
+// const headingStyle = {
+//     color:'red', 
+//     backgroundColor:'black'
+// }
+
+export default Header
